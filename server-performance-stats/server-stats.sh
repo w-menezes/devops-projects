abdd3a6edad69f0a9eb54e8a413f4335b55605d8
William Menezes <william@deb12.home.local>
2024-12-29 01:32:47 -0600

    finished last 2 function

--- server-performance-stats/server-stats.sh
+++ server-performance-stats/server-stats.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/bash
 
 get_cpu_usage() {
+    echo -e "\n### CPU Usage ###"
     top -bn1 | grep "%Cpu(s):" | cut -d ',' -f 4 | awk '{print "CPU Usage: " 100-$1 "%"}'
 }
 
 get_memory_usage() {
+    echo -e "\n### Memory Usage ###"
     free | grep "Mem:" -w \
     | awk '{printf "Total: %.1fGi    |    Used: %.1fGi (%.2f%%)    |    Free: %.1fGi (%.2f%%)\n",$2/1024^2, $3/1024^2, $3/$2 * 100, $4/1024^2, $4/$2 * 100}'
 }
 
 get_disk_usage() {
+    echo -e "\n### Disk Usage ###"
     df -h --total \
     | awk '/^total/ {printf "Total: %.1fG    |    Used: %.1f(%.1f%)    |    Free: %.1f(%.1f%)\n", $2,$3,$5,$4, $4/$2 * 100}'
 }
 
-# TODO: list top 5 proc by CPU usage
-# TODO: list top 5 proc by mem usage
+get_proc_by_cpu() {
+    echo -e "\n### Top 5 Processes by CPU Usage (%) ###"
+    ps -eo pid,comm,%cpu --sort=-%cpu \
+    | head -n 6
+}
+
+get_proc_by_mem() {
+    echo -e "\n### Top 5 Processes by Memory Usage (%) ###"
+    ps -eo pid,comm, %mem --sort=-%mem \
+    | head -n 6
+}
+
 # TODO: format and display stats
